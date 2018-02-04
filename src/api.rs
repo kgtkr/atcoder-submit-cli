5f4a59ae03ebc641f13abaa893de60cd5a534f1a
kgtkr <kgtkr.jp@gmail.com>
2018-02-04 15:40:39 +0900

    from_cookie

--- src/api.rs
+++ src/api.rs
@@ -31,6 +31,38 @@ pub struct User {
   user_name: String,
 }
 
+impl User {
+  fn from_cookie(cookie: &::hyper::header::SetCookie) -> auth::Result<User> {
+    let cookie_map = cookie
+      .iter()
+      .filter_map(|x| Cookie::parse_encoded(x.to_string()).ok())
+      .map(|x| (x.name().to_string(), x.value().to_string()))
+      .collect::<HashMap<_, _>>();
+    Ok(User {
+      session: cookie_map
+        .get("_session")
+        .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?
+        .to_string(),
+      kick_id: cookie_map
+        .get("_kick_id")
+        .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?
+        .to_string(),
+      issue_time: cookie_map
+        .get("_issue_time")
+        .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?
+        .to_string(),
+      user_id: cookie_map
+        .get("_user_id")
+        .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?
+        .to_string(),
+      user_name: cookie_map
+        .get("_user_name")
+        .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?
+        .to_string(),
+    })
+  }
+}
+
 pub fn login(user: &str, pass: &str) -> login::Result<User> {
   let client = reqwest::Client::builder()
     .redirect(reqwest::RedirectPolicy::none())
@@ -42,33 +74,8 @@ pub fn login(user: &str, pass: &str) -> login::Result<User> {
   let cookie = res
     .headers()
     .get::<reqwest::header::SetCookie>()
-    .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?
-    .iter()
-    .filter_map(|x| Cookie::parse_encoded(x.to_string()).ok())
-    .map(|x| (x.name().to_string(), x.value().to_string()))
-    .collect::<HashMap<_, _>>();
-  Ok(User {
-    session: cookie
-      .get("_session")
-      .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?
-      .to_string(),
-    kick_id: cookie
-      .get("_kick_id")
-      .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?
-      .to_string(),
-    issue_time: cookie
-      .get("_issue_time")
-      .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?
-      .to_string(),
-    user_id: cookie
-      .get("_user_id")
-      .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?
-      .to_string(),
-    user_name: cookie
-      .get("_user_name")
-      .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?
-      .to_string(),
-  })
+    .ok_or(auth::Error::from_kind(auth::ErrorKind::Auth))?;
+  Ok(User::from_cookie(&cookie)?)
 }
 
-//pub fn get_tasks(contest: &str, session: &str) -> Result<Vec<(String, String)>, ()> {}
+//pub fn get_tasks(contest: &str, user: User) -> Result<Vec<(String, String)>, ()> {}
